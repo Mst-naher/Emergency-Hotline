224bec502d1cea597e4b11e97f6cb5f802c61429
mst <mst.naher.bd@gmail.com>
2025-08-29 18:28:03 +0100

    added one service card in the history by creating a new div

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // console.log('connected')
 
+
+
+
 function alertMessage() {
   alert("National Emergency called");
 }
@@ -24,14 +27,60 @@ document.getElementById("call-btn-national").addEventListener('click', function 
   const number = getElement("call-number-1").innerText;
 console.log(name, number)
 
-const totalName = getElement("call-list").innerText;
-console.log(totalName)
+const NameList = getElement("call-list").innerText;
+console.log(NameList);
 // currentList = 
 
-getElement("call-list").innerText = totalName;
+getElement("call-list").innerText = NameList;
+
+const callContainer = getElement("call-container");
+const newCall = document.createElement("div");
+newCall.innerHTML = `
+  
+                <div class="m-5 bg-gray-200 rounded-xl p-4">
+                 <h3 class="font-bold">Service's Name</h3>
+                 <h3 class="font-bold "></h3>
+                </div>
+              
+
+`;
+
+callContainer.append("newCall");
+
+
 });
 
 
+document
+  .getElementById("call-btn-national")
+  .addEventListener("click", function () {
+    const name = getElement("card-title-1").innerText;
+    const number = getElement("call-number-1").innerText;
+    console.log(name, number);
+
+    const NameList = getElement("call-list").innerText;
+    console.log(NameList);
+    // currentList =
+
+    getElement("call-list").innerText = NameList;
+
+    const callContainer = getElement("call-container");
+    const newCall = document.createElement("div");
+    newCall.innerHTML = `
+  
+                <div class="m-5 bg-gray-200 rounded-xl p-4">
+                 <h3 class="font-bold">Service's Name</h3>
+                 <h3 class="font-bold "></h3>
+                </div>
+              
+
+`;
+
+    callContainer.append("newCall");
+  });
+
+
+ 
 
 
 
@@ -53,34 +102,3 @@ getElement("call-list").innerText = totalName;
 
 
 
-
-
-
-
-//  const titles = document.getElementsByClassName("card-title");
-// console.log(titles);
-
-//inner content changing:
-// function removeSpace(str){
-//   return str.replaceAll(" ", "").toUpperCase();
-// }
-
-// const titles = document.querySelectorAll(".card-title");
-// for (let title of titles){
-// console.log(title);
-// title.innerText = "We are PH Studernt";
-// title.innerHTML = `<span class='text-pink-500'>${removeSpace(title.innerText)}</span>`;
-// title.style.backgroundColor = 'green';
-// title.style.border = '3px solid red';
-// title.classList.remove("card-title");
-// }
-
-// const images = document.getElementsByTagName("img");
-// console.log(images);
-// for (let img of images) {
-//   img.addEventListener("mouseenter", function () {
-    
-//     img.src =
-//       "https://www.thestudentpocketguide.com/wp-content/uploads/2007/05/Mr-Bean.jpg";
-//   });
-// }
