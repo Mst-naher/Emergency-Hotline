d2f2daa122fbbdadeee2079cb471c02fec801ea2
mst <mst.naher.bd@gmail.com>
2025-08-29 16:50:43 +0100

    added the answers of the assignment and function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,86 @@
 // console.log('connected')
-let count = 0;
+
+function alertMessage() {
+  alert("National Emergency called");
+}
+
+document
+  .getElementById("call-btn-national")
+  .addEventListener("click", function () {
+    alert("National Emergency Number : 999");
+  });
 
 
- const hearBtn = document.getElementById('count-increase-btn')
- const favCount = document
-   .getElementById("fav-count")
+
+ 
+function getElement(id){
+  const element =document.getElementById(id);
+  return element;
+}
+
+document.getElementById("call-btn-national").addEventListener('click', function (){
+
+  const name = getElement("card-title-1").innerText;
+  const number = getElement("call-number-1").innerText;
+console.log(name, number)
+
+const totalName = getElement("call-list").innerText;
+console.log(totalName)
+// currentList = 
+
+getElement("call-list").innerText = totalName;
+});
 
 
-   hearBtn.addEventListener("click", function (e) {
-     e.preventDefault();
-     count++;
-     favCount.innerText = count;
-    //  console.log(count)
-   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const titles = document.getElementsByClassName("card-title");
+// console.log(titles);
+
+//inner content changing:
+// function removeSpace(str){
+//   return str.replaceAll(" ", "").toUpperCase();
+// }
+
+// const titles = document.querySelectorAll(".card-title");
+// for (let title of titles){
+// console.log(title);
+// title.innerText = "We are PH Studernt";
+// title.innerHTML = `<span class='text-pink-500'>${removeSpace(title.innerText)}</span>`;
+// title.style.backgroundColor = 'green';
+// title.style.border = '3px solid red';
+// title.classList.remove("card-title");
+// }
+
+// const images = document.getElementsByTagName("img");
+// console.log(images);
+// for (let img of images) {
+//   img.addEventListener("mouseenter", function () {
+    
+//     img.src =
+//       "https://www.thestudentpocketguide.com/wp-content/uploads/2007/05/Mr-Bean.jpg";
+//   });
+// }
--- readme.md
+++ readme.md
@@ -1,7 +1,20 @@
-#
-.## Answer the following questions clearly:
-- What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
-- How do you create and insert a new element into the DOM?
-- What is Event Bubbling and how does it work?
-- What is Event Delegation in JavaScript? Why is it useful?
-- What is the difference between preventDefault() and stopPropagation() methods?
+
+## Answers : 
+ 
+1. 
+-  Brings an element into the DOM through its unique name by `ID`.
+-  Brings an element into the DOM through by its `className` name .
+- Selects the `CSS` selector and find-out first css element by this css selector.
+- This brings an array of elements which is a `nodeList`.
+
+2. At first, catch an element by its `ID` and bring it to the `DOM`.
+After that, keep it in a new variable with let or const. And then add .innerText to it. 
+` const newCart = document.createElement('div') `
+
+3. When a user clicks a place or a button, then the `Event Bubbling ` starts and goes to find out the clicking source step by step. Like it will go first, it's parent ➡ parent ➡ child ➡child[0] etc. Finally, the Event Bubbling detects the clicked point
+
+4. ` Event Delegation is the best event.` Because it does not use Lup to run the Function. It increases the efficiency of the code base and decreases the Browser's load.
+
+ 5. `preventDefault()` It stops the default function.
+`stopPropagation()`  It stops the Event Bubbling.
+
